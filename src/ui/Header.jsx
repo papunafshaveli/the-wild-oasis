29ad19a7112424e12923a71e3adfc361f29b1fd1
Papuna Fshaveli <papunafshaveli@gmail.com>
2023-11-13 17:15:14 +0400

    add test version

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+  background-color: var(--color-grey-0);
+  padding: 1.2rem 4.8rem;
+  border-bottom: 1px solid var(--color-grey-100);
+`;
+
+export default function Header() {
+  return <StyledHeader>Header</StyledHeader>;
+}
---
+++ src/ui/SideBar.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import styled from "styled-components";
+
+const StyledSidebar = styled.aside`
+  background-color: var(--color-grey-0);
+  padding: 3.2rem 2.4rem;
+  border-right: 1px solid var(--color-grey-100);
+
+  grid-row: 1 / -1;
+`;
+
+export default function SideBar() {
+  return <StyledSidebar>Sidebar</StyledSidebar>;
+}
